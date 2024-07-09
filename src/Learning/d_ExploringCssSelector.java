02f0301bc6ffa77c66ab5ec46969c8cccd7167a6
HenilMistry <henilmistry74496@gmail.com>
2024-07-09 10:21:51 +0530

    added an example to demonstrate selecion of web element by Css Selector

---
+++ src/Learning/d_ExploringCssSelector.java
@@ -0,0 +1,33 @@
+package Learning;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * This class extends default example, and it will
+ * show you that how you can user Css Selector to
+ * grab web elements and perform some actions.
+ * */
+public class d_ExploringCssSelector extends defaultExample {
+
+    public d_ExploringCssSelector() {
+        // loading the site...
+        driver.get(SiteUrl);
+        // creating anonymous object and selecting by visible text...
+        new Select(this.driver.findElement(By.cssSelector("#ddlInst"))).selectByVisibleText("CSPIT");
+        new Select(this.driver.findElement(By.cssSelector("#ddlDegree"))).selectByVisibleText("BTECH(CE)");
+        new Select(this.driver.findElement(By.cssSelector("#ddlSem"))).selectByVisibleText("8");
+        new Select(this.driver.findElement(By.cssSelector("#ddlScheduleExam"))).selectByVisibleText("APRIL 2024");
+        // finding element by id and sending some data in field...
+        driver.findElement(By.id("txtEnrNo")).sendKeys("20CE054");
+        // finding element by id and clicking on it...
+        driver.findElement(By.id("btnSearch")).click();
+
+        this.waitAndCloseDriver();
+    }
+
+    public static void main(String[] args) {
+        new d_ExploringCssSelector();
+    }
+
+}
